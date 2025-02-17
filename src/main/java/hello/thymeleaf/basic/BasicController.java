f3ae97b488e33f8c072f2922a6a9172d38c99d9c
HarryBae1011 <06harry@naver.com>
2025-02-17 15:24:47 +0900

    템플릿 조각

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -131,7 +131,6 @@ public class BasicController {
         return "basic/javascript";
     }
 
-
     private void addUsers(Model model) {
         List<User> list = new ArrayList<>();
         list.add(new User("UserA", 10));
